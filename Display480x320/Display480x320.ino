97450d227bf3fcf0a8b9e78ad1e5a040cb1a6dfa
Vasily <gltver@yandex.ru>
2020-09-28 23:49:55 +0300

    написал прерывание

--- Display480x320/Display480x320.ino
+++ Display480x320/Display480x320.ino
@@ -14,19 +14,20 @@ void setup()
 	Serial.begin(115200);
 
 }
-byte indx = 0;
-char* p_c;
-int* p_i;
-byte mass[100];
-byte buffer[6];
+volatile byte buffer[6];
+volatile byte count = 0;
+volatile byte data = 0;
+volatile byte flag = 0;
 
-byte count = 0;
+int angle;
+int coord_X;
+int coord_Y;
 
 // SPI interrupt routine
 
 ISR(SPI_STC_vect)
 {
-	byte data = SPDR;
+	data = SPDR;
 	switch (count)
 	{
 		case 0 :
@@ -84,62 +85,53 @@ ISR(SPI_STC_vect)
 		{
 			buffer[5] = data;
 			count = 0;             // Приняли все данные, начинаем ждать новый правильный байт
-			int *p = (int*)(&buffer);         // Serial.print("buffer: "); Serial.println(*pI); 
-			int angle = *p;
-			*p++;
-			int coord_X = *p;
-			*p++;
-			int coord_Y = *p;
+			int* p = (int*)(&buffer);        // обьявляем переменную
+			angle = *p++;
+			coord_X = *p++;
+			coord_Y = *p;
+			flag = 1;
 			return;
 		}
 	}
 
 
 
-	if (count == 0 && c == 0x1A) //Ждем первый случебный байт при нулевом счетчике
-	{
-		count++;                     // Увеличиваем счетчик, что значит что пришел первый байт
-		return;
-	}
-	else
-	{
-		if (count == 1 && c == 0x1B)// Сюда когда был приняьт первый байт и пришел второй правильный   или что угодно
-		{
-			count++;                     // Увеличиваем счетчик, что значит что пришел второй байт
-			return;
-		}
-		else
-		{
-			count = 0;                     // Пришел второй байт не правильный. сбрасываем счетчик и начинаем все сначала
-			return;
-		}
-
-	}
 
 
 
-
-
-	mass[indx] = c;
-	indx++;
-	if (indx > 1)
-	{
-		indx = 0;
-		data = ((int16_t)mass[0] << 8) | mass[1];             // Сдвигаем старший байт влево и добавляем младший байт  // Возвращаем значение 
-	 //   Serial.println(mass[0],BIN);
-	 //   Serial.println(mass[1],BIN);
-		Serial.println(data, BIN);
-		Serial.println("---");
-	}
+	//mass[indx] = c;
+	//indx++;
+	//if (indx > 1)
+	//{
+	//	indx = 0;
+	//	data = ((int16_t)mass[0] << 8) | mass[1];             // Сдвигаем старший байт влево и добавляем младший байт  // Возвращаем значение 
+	// //   Serial.println(mass[0],BIN);
+	// //   Serial.println(mass[1],BIN);
+	//	Serial.println(data, BIN);
+	//	Serial.println("---");
+	//}
 
 }
 
 void loop()
 {
-	//    Serial.println(mass[0],BIN);
-	//    Serial.println(mass[1],BIN);
-	//    Serial.println(data,BIN);
-	   // Serial.println(data);
-	delay(100);
+		if (flag ==1)
+		{
+			//Serial.print("angle byte ");
+			Serial.print(angle);
+			Serial.print(" ");
+			//Serial.print("coord_X byte ");
+			Serial.print(coord_X);
+			Serial.print(" ");
+			//Serial.print("coord_Y byte ");
+			Serial.print(coord_Y);
+			Serial.print(" = ");Serial.println(b-a);
+		}
+
+
+
+
+
+	delay(50);
 
 }
